aa037ca232101a906d2d4be1003d8913c4b7352a
Phil Cooper-King <phil@cooperking.net>
2017-01-17 07:45:47 +0000

    added last.

--- unit_tests/buffer.cpp
+++ unit_tests/buffer.cpp
@@ -7,31 +7,31 @@
 TEST_CASE("Buffer")
 {
 
-  buffer::buffer somebuffer;
+  util::buffer::data somebuffer;
 
   SECTION("Un-initialized buffer")
   {
-    REQUIRE(buffer::size(&somebuffer) == 0);
-    REQUIRE(buffer::capacity(&somebuffer) == 0); 
-    REQUIRE(buffer::empty(&somebuffer) == true);
+    REQUIRE(util::buffer::size(&somebuffer) == 0);
+    REQUIRE(util::buffer::capacity(&somebuffer) == 0);
+    REQUIRE(util::buffer::empty(&somebuffer) == true);
   }
 
   SECTION("Initialize Buffer")
   {
-    buffer::init(
+    util::buffer::init(
       &somebuffer,
       sizeof(int),
       1
     );
 
-    REQUIRE(buffer::size(&somebuffer) == 0);
-    REQUIRE(buffer::capacity(&somebuffer) == 1); 
-    REQUIRE(buffer::empty(&somebuffer) != false);
+    REQUIRE(util::buffer::size(&somebuffer) == 0);
+    REQUIRE(util::buffer::capacity(&somebuffer) == 1);
+    REQUIRE(util::buffer::empty(&somebuffer) != false);
   }
 
   SECTION("Push")
   {
-    buffer::init(
+    util::buffer::init(
       &somebuffer,
       sizeof(int),
       1
@@ -39,26 +39,26 @@ TEST_CASE("Buffer")
 
     int *array = nullptr;
 
-    buffer::push(&somebuffer);
-    array = (int*)buffer::data(&somebuffer);
+    util::buffer::push(&somebuffer);
+    array = (int*)util::buffer::bytes(&somebuffer);
     array[0] = 1;
 
-    REQUIRE(buffer::size(&somebuffer) == 1);
-    REQUIRE(buffer::capacity(&somebuffer) == 1);
+    REQUIRE(util::buffer::size(&somebuffer) == 1);
+    REQUIRE(util::buffer::capacity(&somebuffer) == 1);
 
-    buffer::push(&somebuffer);
-    array = (int*)buffer::data(&somebuffer);
+    util::buffer::push(&somebuffer);
+    array = (int*)util::buffer::bytes(&somebuffer);
     array[1] = 2;
 
-    REQUIRE(buffer::size(&somebuffer) == 2);
-    REQUIRE(buffer::capacity(&somebuffer) == 2);
+    REQUIRE(util::buffer::size(&somebuffer) == 2);
+    REQUIRE(util::buffer::capacity(&somebuffer) == 2);
 
-    buffer::push(&somebuffer);
-    array = (int*)buffer::data(&somebuffer);
+    util::buffer::push(&somebuffer);
+    array = (int*)util::buffer::bytes(&somebuffer);
     array[2] = 3;
 
-    REQUIRE(buffer::size(&somebuffer) == 3);
-    REQUIRE(buffer::capacity(&somebuffer) == 4);
+    REQUIRE(util::buffer::size(&somebuffer) == 3);
+    REQUIRE(util::buffer::capacity(&somebuffer) == 4);
 
     REQUIRE(array[0] == 1);
     REQUIRE(array[1] == 2);
@@ -67,68 +67,68 @@ TEST_CASE("Buffer")
 
   SECTION("Resize")
   {
-    buffer::init(
+    util::buffer::init(
       &somebuffer,
       sizeof(int),
       1
     );
-    
-    REQUIRE(buffer::capacity(&somebuffer) == 1);
 
-    buffer::resize(&somebuffer, 32);
+    REQUIRE(util::buffer::capacity(&somebuffer) == 1);
 
-    REQUIRE(buffer::capacity(&somebuffer) == 32);
+    util::buffer::resize(&somebuffer, 32);
 
-    int *array = (int*)buffer::data(&somebuffer);
+    REQUIRE(util::buffer::capacity(&somebuffer) == 32);
+
+    int *array = (int*)util::buffer::bytes(&somebuffer);
     for(uint32_t i = 0; i < 32; ++i)
     {
       array[i] = i;
     }
 
-    buffer::resize(&somebuffer, 64);
+    util::buffer::resize(&somebuffer, 64);
 
-    REQUIRE(buffer::capacity(&somebuffer) == 64);
-    array = (int*)buffer::data(&somebuffer);
+    REQUIRE(util::buffer::capacity(&somebuffer) == 64);
+    array = (int*)util::buffer::bytes(&somebuffer);
 
     for(uint32_t i = 0; i < 32; ++i)
     {
-      REQUIRE(i == array[i]);   
-    }   
+      REQUIRE(i == array[i]);
+    }
   }
 
   SECTION("Reserve")
   {
-    buffer::init(
+    util::buffer::init(
       &somebuffer,
       sizeof(int),
       1
     );
 
-    buffer::reserve(&somebuffer, 32);
+    util::buffer::reserve(&somebuffer, 32);
 
-    REQUIRE(buffer::capacity(&somebuffer) == 32);
-    REQUIRE(buffer::size(&somebuffer) == 0);
+    REQUIRE(util::buffer::capacity(&somebuffer) == 32);
+    REQUIRE(util::buffer::size(&somebuffer) == 0);
   }
 
   SECTION("Insert")
   {
-    buffer::init(
+    util::buffer::init(
       &somebuffer,
       sizeof(int),
       4
     );
 
-    buffer::push(&somebuffer);
-    buffer::push(&somebuffer);
-    buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
 
-    int *array = (int*)buffer::data(&somebuffer);
+    int *array = (int*)util::buffer::bytes(&somebuffer);
     array[0] = 1;
     array[1] = 2;
-    array[2] = 3; 
-  
-    buffer::insert(&somebuffer, 1);
-    array = (int*)buffer::data(&somebuffer);
+    array[2] = 3;
+
+    util::buffer::insert(&somebuffer, 1);
+    array = (int*)util::buffer::bytes(&somebuffer);
 
     REQUIRE(array[0] == 1);
     REQUIRE(array[2] == 2);
@@ -137,42 +137,71 @@ TEST_CASE("Buffer")
 
   SECTION("Erase")
   {
-    buffer::init(
+    util::buffer::init(
       &somebuffer,
       sizeof(int),
       4
     );
 
-    buffer::push(&somebuffer);
-    buffer::push(&somebuffer);
-    buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
 
-    int *array = (int*)buffer::data(&somebuffer);
+    int *array = (int*)util::buffer::bytes(&somebuffer);
     array[0] = 1;
     array[1] = 2;
     array[2] = 3;
-  
-    buffer::erase(&somebuffer, 1);
+
+    util::buffer::erase(&somebuffer, 1);
 
     REQUIRE(array[1] == 3);
   }
 
   SECTION("clear")
   {
-    buffer::init(
+    util::buffer::init(
       &somebuffer,
       sizeof(int),
       4
     );
 
-    buffer::push(&somebuffer);
-    buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
 
-    REQUIRE(buffer::size(&somebuffer) == 2);
+    REQUIRE(util::buffer::size(&somebuffer) == 2);
 
-    buffer::clear(&somebuffer);
+    util::buffer::clear(&somebuffer);
 
-    REQUIRE(buffer::size(&somebuffer) == 0);
+    REQUIRE(util::buffer::size(&somebuffer) == 0);
+  }
+
+  SECTION("last")
+  {
+    util::buffer::init(
+      &somebuffer,
+      sizeof(int),
+      4
+    );
+
+    int *array = (int*)util::buffer::last(&somebuffer);
+
+    REQUIRE(array == nullptr);
+
+    util::buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
+    util::buffer::push(&somebuffer);
+    array = (int*)util::buffer::last(&somebuffer);
+
+    REQUIRE(array != nullptr);
+
+    array = (int*)util::buffer::bytes(&somebuffer);
+    array[0] = 1;
+    array[1] = 2;
+    array[2] = 3;
+    array[3] = 4;
+
+    array = (int*)util::buffer::last(&somebuffer);
+    REQUIRE(*array == 4);
   }
 }
-
