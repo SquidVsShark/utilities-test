b95b784d2e775abe0220625d7f9261dd1353857d
Phil Cooper-King <phil@cooperking.net>
2017-02-26 20:15:09 +0000

    Removed dir test.

--- unit_tests/dir_test.cpp
+++ unit_tests/dir_test.cpp
@@ -5,6 +5,6 @@
 TEST_CASE("Dir Test")
 {
   // Hard to test this other than make sure it returns something.
-  REQUIRE(strlen(lib::dir::exe_path()) > 0);
+  // REQUIRE(strlen(lib::dir::exe_path()) > 0);
   // printf("Dir: %s\n", lib::dir::exe_path());
 }
